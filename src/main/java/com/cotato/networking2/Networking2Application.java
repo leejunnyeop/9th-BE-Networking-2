5d7139e8dc1f7f3854f5f46207afa52b1bdc9411
qjvk2880 <qjvk2880@gmail.com>
2024-03-25 17:27:17 +0900

    Initial Commit

---
+++ src/main/java/com/cotato/networking2/Networking2Application.java
@@ -0,0 +1,13 @@
+package com.cotato.networking2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Networking2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Networking2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cotato/networking2/Networking2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotato.networking2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Networking2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
